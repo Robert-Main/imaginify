1b765e9f0e643ab390a213c7ee439e4c04184381
robert-devs <mainrobert04@gmail.com>
2024-02-20 19:49:35 +0300

    updates on middleware

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
 	publicRoutes: [
 		'/',
-		'/api/webhook/clerk',
+		'/api/webhooks/clerk',
 		'/api/webhook/stripe',
 		'/api/uploadthing'
 	],
