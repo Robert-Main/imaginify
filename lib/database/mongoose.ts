65bf82920aa51ccc91ef1212fd4e16cc509d6665
robert-devs <mainrobert04@gmail.com>
2024-02-20 18:38:30 +0300

    updates

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,18 +1,30 @@
-import mongoose from 'mongoose';
+import mongoose, { Mongoose } from 'mongoose';
 
 const MONGODB_URL = process.env.MONGODB_URL;
 
-let cached = (global as any).mongoose || { conn: null, promise: null };
+interface MongooseConnection {
+	conn: Mongoose | null;
+	promise: Promise<Mongoose> | null;
+}
+
+let cached: MongooseConnection = (global as any).mongoose;
+
+if (!cached) {
+	cached = (global as any).mongoose = {
+		conn: null, promise: null
+	};
+}
 
 export const connectToDatabase = async () => {
 	if (cached.conn) return cached.conn;
 
-	if (!MONGODB_URL) throw new Error(`MONGODB_URL  is missing`);
+	if (!MONGODB_URL) throw new Error('Missing MONGODB_URL');
 
-	cached.promise = cached.promise || mongoose.connect(MONGODB_URL, {
-		dbName: "imaginify",
-		bufferCommands: false,
-	});
+	cached.promise =
+		cached.promise ||
+		mongoose.connect(MONGODB_URL, {
+			dbName: 'imagefy', bufferCommands: false
+		});
 
 	cached.conn = await cached.promise;
 
