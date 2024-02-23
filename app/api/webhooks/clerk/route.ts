7e184e674343dfa114645c162f53a605189ca842
robert-devs <mainrobert04@gmail.com>
2024-02-23 12:23:47 +0300

    updates

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -9,7 +9,7 @@ import { Webhook } from "svix";
 
 export async function POST(req: Request) {
 	// You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
-	const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+	const WEBHOOK_SECRET = "whsec_WMf0JoEk3BL+1wjba5wLCbeLry9+392R";
 
 	if (!WEBHOOK_SECRET) {
 		throw new Error(
